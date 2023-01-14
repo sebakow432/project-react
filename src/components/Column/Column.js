d7f842f6f90a9d7911c401e28556cabe436a1a8d
Sebastian Kowalski <wwy.skowalski@wp.pl>
2023-01-14 21:15:31 +0100

    Add Module 11.7

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,22 @@
 import styles from './Column.module.scss'
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
 
 const Column = props => {
-    return (
-        <article className={styles.column}>
-            <h2 className={styles.title}>
-                {props.title}
-                <span className={styles.icon + ' fa fa-' + props.icon}>
-                
-                </span>
-            </h2>
-        </article>
-    );
+  return (
+    <article className={styles.column}>
+      <h2 className={styles.title}>
+        {props.title}
+        <span className={styles.icon + ' fa fa-' + props.icon}>
+        </span>
+      </h2>
+      <ul className={styles.cards}>
+	      {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+      </ul>
+      <CardForm columnId={props.id} action={props.action} />
+      
+    </article>
+  );
 };
 
 export default Column;
