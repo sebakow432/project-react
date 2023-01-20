41b387e4326c3f28709956f5be04b6f64bffebdc
Sebastian Kowalski <wwy.skowalski@wp.pl>
2023-01-20 17:43:38 +0100

    Add Module 13.3

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,15 +2,19 @@ import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const CardForm = props => {
     const [title, setTitle] = useState('');
+
+    const dispatch = useDispatch();
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        const columnId = props.columnId;
+        dispatch({type: 'ADD_CARD', payload: { title, columnId } });
         setTitle('');
     };
-    
+
 	return (
         <form className={styles.cardForm} onSubmit={handleSubmit}>
             <TextInput value={title} onChange={e => setTitle(e.target.value)} />
@@ -20,7 +24,3 @@ const CardForm = props => {
 };
 
 export default CardForm;
-
-/*
-TextInput({value: title})
-*/
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import App from './App';
@@ -7,12 +7,11 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.querySelector('#root')
-);
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+<React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+</React.StrictMode>);
 
